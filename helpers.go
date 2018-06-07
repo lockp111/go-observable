51c3f79de826012222fb0d627e8a1fc6156e93b8
harman <lockp111@icloud.com>
2018-06-07 13:49:52 +0800

    fix:delete event bugs

--- helpers.go
+++ helpers.go
@@ -1,8 +1,8 @@
 package observable
 
 import (
-  "reflect"
-  "strings"
+	"reflect"
+	"strings"
 )
 
 // Helpers
@@ -10,91 +10,93 @@ import (
 // Add a callback under a certain event namespace
 func (o *Observable) addCallback(event string, cb interface{}, isUnique bool) *Observable {
 
-  fn := reflect.ValueOf(cb)
-  events := strings.Fields(event)
-  isTyped := len(events) > 1
+	fn := reflect.ValueOf(cb)
+	events := strings.Fields(event)
+	isTyped := len(events) > 1
 
-  for _, s := range events {
-    o.Lock()
-    // does this namespace already exist?
-    if !o.hasEvent(s) {
-      o.Callbacks[s] = make([]callback, 1)
-      o.Callbacks[s][0] = callback{fn, isUnique, isTyped, false}
-    } else {
-      o.Callbacks[s] = append(o.Callbacks[s], callback{fn, isUnique, isTyped, false})
-    }
-    o.Unlock()
-  }
+	for _, s := range events {
+		o.Lock()
+		// does this namespace already exist?
+		if !o.hasEvent(s) {
+			o.Callbacks[s] = make([]callback, 1)
+			o.Callbacks[s][0] = callback{fn, isUnique, isTyped, false}
+		} else {
+			o.Callbacks[s] = append(o.Callbacks[s], callback{fn, isUnique, isTyped, false})
+		}
+		o.Unlock()
+	}
 
-  return o
+	return o
 }
 
 // remove the events bound to the callback
 func (o *Observable) removeEvent(event string, fn interface{}) {
 
-  events := strings.Fields(event)
-  // try to get the value of the function we want unsubscribe
-  fn = reflect.ValueOf(fn)
+	events := strings.Fields(event)
+	// try to get the value of the function we want unsubscribe
+	fn = reflect.ValueOf(fn)
 
-  o.Lock()
-  for _, s := range events {
-    // loop all the callbacks registered under the event namespace
-    for i, cb := range o.Callbacks[s] {
-      if fn == cb.fn {
-        o.Callbacks[s] = append(o.Callbacks[s][:i], o.Callbacks[s][i+1:]...)
-      }
-    }
-    // if there are no more callbacks using this namespace
-    // delete the key from the map
-    if len(o.Callbacks[event]) == 0 {
-      delete(o.Callbacks, event)
-    }
-  }
-  o.Unlock()
+	for _, s := range events {
+		o.Lock()
+		if o.hasEvent(s) {
+			// loop all the callbacks registered under the event namespace
+			for i, cb := range o.Callbacks[s] {
+				if fn == cb.fn {
+					o.Callbacks[s] = append(o.Callbacks[s][:i], o.Callbacks[s][i+1:]...)
+				}
+			}
+			// if there are no more callbacks using this namespace
+			// delete the key from the map
+			if len(o.Callbacks[s]) == 0 {
+				delete(o.Callbacks, s)
+			}
+		}
+		o.Unlock()
+	}
 
 }
 
 // dispatch the events using custom arguments
 func (o *Observable) dispatchEvent(event string, arguments []reflect.Value) *Observable {
-  // lock the struct
-  o.Lock()
-  defer o.Unlock()
+	// lock the struct
+	o.Lock()
+	defer o.Unlock()
 
-  // get all the list of events space separated
-  events := strings.Fields(event)
+	// get all the list of events space separated
+	events := strings.Fields(event)
 
-  for _, s := range events {
-    // check if the observable has already created this events map
-    if o.hasEvent(s) {
+	for _, s := range events {
+		// check if the observable has already created this events map
+		if o.hasEvent(s) {
 
-      // loop all the callbacks
-      // avoiding to call twice the ones registered with Observable.One
-      for i, cb := range o.Callbacks[s] {
+			// loop all the callbacks
+			// avoiding to call twice the ones registered with Observable.One
+			for i, cb := range o.Callbacks[s] {
 
-        if !cb.isUnique || cb.isUnique && !cb.wasCalled {
-          // if the callback was registered with multiple events
-          // we prepend the event namespace to the function arguments
-          if cb.isTyped {
-            cb.fn.Call(append([]reflect.Value{reflect.ValueOf(s)}, arguments...))
-          } else {
-            cb.fn.Call(arguments)
-          }
-        }
-        // kill the callbacks registered with one
-        if cb.isUnique {
-          o.Off(s, o.Callbacks[s][i])
-        }
+				if !cb.isUnique || cb.isUnique && !cb.wasCalled {
+					// if the callback was registered with multiple events
+					// we prepend the event namespace to the function arguments
+					if cb.isTyped {
+						cb.fn.Call(append([]reflect.Value{reflect.ValueOf(s)}, arguments...))
+					} else {
+						cb.fn.Call(arguments)
+					}
+				}
+				// kill the callbacks registered with one
+				if cb.isUnique {
+					o.Off(s, o.Callbacks[s][i])
+				}
 
-        o.Callbacks[s][i].wasCalled = true
-      }
-    }
-  }
+				o.Callbacks[s][i].wasCalled = true
+			}
+		}
+	}
 
-  return o
+	return o
 }
 
 // check whether the Observable struct has already registered the event namespace
 func (o *Observable) hasEvent(event string) bool {
-  _, ok := o.Callbacks[event]
-  return ok
+	_, ok := o.Callbacks[event]
+	return ok
 }
--- readme.md
+++ readme.md
@@ -10,7 +10,7 @@ It allows you to send and receive events with a tiny simple API (thread safe)
 ## Installation
 
 ```go
-go get github.com/GianlucaGuarini/go-observable
+go get github.com/lockp111/go-observable
 ```
 
 ## Api
